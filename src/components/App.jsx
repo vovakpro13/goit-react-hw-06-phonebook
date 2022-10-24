2df1c539050294eb5ea5dce5f13835f3658108c9
VolodymyrMelnykInoxoft <volodymyr.melnyk@inoxoft.com>
2022-10-24 22:50:07 +0300

    initial

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
