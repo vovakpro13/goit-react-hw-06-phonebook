ffefa48a6dbb4389ae7dd43c285e84e5abd061e8
VolodymyrMelnykInoxoft <volodymyr.melnyk@inoxoft.com>
2022-10-25 23:01:18 +0300

    write contacts to the localstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import AddContactForm from 'components/AddContactForm';
 import Filter from 'components/Filter';
 import ContactList from 'components/ContactList';
 import { defaultContacts } from 'constants/defaultContacts';
+import { LOCALSTORAGE_CONTACTS_KEY } from '../constants/common';
 
 class App extends Component {
   constructor(props) {
@@ -18,14 +19,34 @@ class App extends Component {
     this.handleDeleteContact = this.handleDeleteContact.bind(this);
   }
 
+  componentDidMount() {
+    const contactsFromStorage = localStorage.getItem(LOCALSTORAGE_CONTACTS_KEY);
+
+    if (contactsFromStorage) {
+      const parsedContacts = JSON.parse(contactsFromStorage);
+      return this.setState({ contacts: parsedContacts });
+    }
+
+    localStorage.setItem(LOCALSTORAGE_CONTACTS_KEY, JSON.stringify([]));
+  }
+
+  writeContactsToLocalStorage(contacts) {
+    const jsonContacts = JSON.stringify(contacts);
+    localStorage.setItem(LOCALSTORAGE_CONTACTS_KEY, jsonContacts);
+  }
+
   handleAddNewContact(newContact) {
     if (this.state.contacts.some(({ name }) => name === newContact.name)) {
       return alert(`${newContact.name} is already in contacts.`);
     }
 
-    this.setState(({ contacts }) => ({
-      contacts: [{ id: nanoid(10), ...newContact }, ...contacts],
-    }));
+    this.setState(({ contacts }) => {
+      const newContacts = [{ id: nanoid(10), ...newContact }, ...contacts];
+
+      this.writeContactsToLocalStorage(newContacts);
+
+      return { contacts: newContacts };
+    });
   }
 
   handleChangeFilter({ target: { value: filter } }) {
@@ -33,9 +54,13 @@ class App extends Component {
   }
 
   handleDeleteContact(deleteId) {
-    this.setState(({ contacts }) => ({
-      contacts: contacts.filter(({ id }) => deleteId !== id),
-    }));
+    this.setState(({ contacts }) => {
+      const newContacts = contacts.filter(({ id }) => deleteId !== id);
+
+      this.writeContactsToLocalStorage(newContacts);
+
+      return { contacts: newContacts };
+    });
   }
 
   render() {
